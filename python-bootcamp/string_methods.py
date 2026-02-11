59f601340249052fd90b7ce25a7f393b7b84a9eb
Kirill Chehodaiev <Kirill.Chehodaiev@ellucian.com>
2026-02-11 15:09:03 +0000

    dummy 5

--- python-bootcamp/string_methods.py
+++ python-bootcamp/string_methods.py
@@ -1,4 +1,9 @@
 # String methods
+# String methods are essentially a functions attached to each object data type (str, int, float, set, dictionary, etc).
+# Meaning there specific methods for each datatype.
 
+# print(), len(), type(), sum(), max(), main(), round() etc these are regular functions, not methods
 
-'a'.
+# 'dfdfsf'.  First method to call all available string methods - just type a literal string, then dot and then there will be a dropdown.
+
+print(dir(str)) # second method to output all avail method for a data type of string
