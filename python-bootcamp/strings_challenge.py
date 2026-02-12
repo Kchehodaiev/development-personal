f21577047eb93303fdb41a45a836335a2e616a31
Kirill Chehodaiev <Kirill.Chehodaiev@ellucian.com>
2026-02-12 13:39:39 +0000

    dummy 6

--- python-bootcamp/strings_challenge.py
+++ python-bootcamp/strings_challenge.py
@@ -18,9 +18,41 @@
 
 
 # Calculate the BMI (Body Mass Index) based on the user input for weight in kg and height in meters. The formula for BMI is weight / (height ** 2). Display the result rounded to 2 decimal places.
-weight = float(input('Enter your fat-ass kg:'))
-height = float(input('Enter your hieght in meters:'))
-bmi = weight / (height ** 2)
-print('Your BMI is:', round(bmi, 2)) 
-# Or like this:
-print('Your BMI is: ', format(bmi, '.2f'))
+# weight = float(input('Enter your fat-ass kg:'))
+# height = float(input('Enter your hieght in meters:'))
+# bmi = weight / (height ** 2)
+# print('Your BMI is:', round(bmi, 2)) 
+# # Or like this:
+# print('Your BMI is: ', format(bmi, '.2f'))
+
+
+"""
+Consider the following string declaration which is part of the output of a Linux command.
+
+my_str = 'wlo1 Link encap:Ethernet HWaddr b4:6d:83:77:85:f3'
+
+Write a Python script that extracts the MAC address (b4:6d:83:77:85:f3) from the string.
+
+Try to solve the challenge in more than one way.
+"""
+my_str = 'wlo1 Link encap:Ethernet HWaddr b4:6d:83:77:85:f3'
+mac_str = my_str.removeprefix('wlo1 Link encap:Ethernet HWaddr').strip()
+
+print(mac_str) # prints out 'b4:6d:83:77:85:f3'
+# Split the string into a list and return the last list element which is the MAC address
+mac = my_str.split()[-1]
+print(mac)
+# Solution to revert the string backwards and slice the reverted mac out.
+print(my_str[::-1])
+print(my_str[:-18:-1])
+# then we need to convert the resulted string to list and revert it back to string
+reverted_mac = my_str[:-18:-1]
+mac = reverted_mac[::-1]
+print(mac)
+
+# Solution 3
+print(my_str)
+print(len(my_str)) # 49 indexes
+mac = my_str[len(my_str) - 17:]
+print(mac)
+
